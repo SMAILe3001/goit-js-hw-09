e04a42c6e8dcaf0e5975560691c7d067245ae6e8
SMAILe3001 <smaile3001@gmail.com>
2023-03-21 23:06:29 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const bodyEl = document.body;
+const btnColorStart = document.querySelector('[data-start]');
+const btnColorStop = document.querySelector('[data-stop]');
+let identifier = 0;
 
+btnColorStart.addEventListener('click', colorChangeStart);
+btnColorStop.addEventListener('click', colorChangeStop);
+btnColorStop.disabled = true;
+
+function colorChangeStart() {
+  identifier = setInterval(() => {
+    bodyEl.style.background = getRandomHexColor();
+  }, 2000);
+
+  btnColorStart.disabled = true;
+  btnColorStop.disabled = false;
+}
+
+function colorChangeStop() {
+  clearInterval(identifier);
+
+  btnColorStop.disabled = true;
+  btnColorStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

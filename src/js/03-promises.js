96a3a834ef8f27e302935830672f7c4fb40c963b
SMAILe3001 <smaile3001@gmail.com>
2023-03-22 12:39:17 +0200

    Task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
+
+  promise
+    .then(() => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(() => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const firstDelay = Number(formEl.querySelector('input[name="delay"]').value);
+  const stepDelay = Number(formEl.querySelector('input[name="step"]').value);
+  const amount = Number(formEl.querySelector('input[name="amount"]').value);
+  let premixExecutionDelay = firstDelay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, premixExecutionDelay);
+    premixExecutionDelay = firstDelay + stepDelay * i;
   }
+  formEl.reset();
 }
